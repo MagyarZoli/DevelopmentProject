6ead9378288f2ac499b05ba4f4db53ac844684c4
MagyarZoli <magyarz95@gmail.com>
2024-08-04 10:30:20 +0200

    AbstractFactory (#5)

---
+++ Java/DesignPatterns/src/main/java/designPatterns/creational/abstractFactory/AbstractProduct.java
@@ -0,0 +1,11 @@
+package designPatterns.creational.abstractFactory;
+
+public interface AbstractProduct {
+  interface A {
+    String displayA();
+  }
+
+  interface B {
+    String displayB();
+  }
+}
