e6ee5a01ea6070e18d1088392b128546ea996d5c
MagyarZoli <magyarz95@gmail.com>
2024-08-08 06:32:22 +0200

    Composite Test (#10)

---
+++ Java/DesignPatterns/src/test/java/designPatterns/structural/composite/CompositeTest.java
@@ -0,0 +1,26 @@
+package designPatterns.structural.composite;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CompositeTest {
+  private Leaf.A leafA;
+  private Leaf.B leafB;
+  private Composite composite;
+
+  @BeforeEach
+  void setUp() {
+    leafA = new Leaf.A("leafA", 100);
+    leafB = new Leaf.B("leafB", 101, true);
+    composite = new Composite();
+    composite.addComponent(leafA);
+    composite.addComponent(leafB);
+  }
+
+  @Test
+  void equalsTest() {
+    assertThat(composite.show()).isEqualTo("leafA, 100\nleafB, 101, true\n");
+  }
+}
