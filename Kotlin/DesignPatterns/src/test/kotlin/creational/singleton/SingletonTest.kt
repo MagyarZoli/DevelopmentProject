8c150a7477c7e72652d6f57572f1b6f6d871c8eb
MagyarZoli <magyarz95@gmail.com>
2024-09-07 08:31:44 +0200

    Singleton Test #33

---
+++ Kotlin/DesignPatterns/src/test/kotlin/creational/singleton/SingletonTest.kt
@@ -0,0 +1,23 @@
+package creational.singleton
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class SingletonTest {
+
+  private lateinit var s1: Singleton
+  private lateinit var s2: Singleton
+
+  @BeforeEach
+  internal fun setUp() {
+    s1 = Singleton.getInstance()!!
+    s2 = Singleton.getInstance()!!
+  }
+
+  @Test
+  internal fun equalsTest() {
+    assertThat(s1).isEqualTo(s2)
+    assertThat(s1.date).isEqualTo(s2.date)
+  }
+}
