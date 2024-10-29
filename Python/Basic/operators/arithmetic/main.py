a39f7b10e55751aabb58c0d591639413cbce7a96
MagyarZoli <magyarz95@gmail.com>
2024-10-29 06:13:43 +0100

    Arithmetic #56

---
+++ Python/Basic/operators/arithmetic/main.py
@@ -0,0 +1,30 @@
+x = 5
+y = 6
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+modulus = x % y
+exponentiation = x ** y
+floor_division = x // y
+
+bitwise_and = x & y
+bitwise_or = x | y
+bitwise_xor = x ^ y
+bitwise_right_shift = x >> y
+bitwise_left_shift = x << y
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulus)
+print(exponentiation)
+print(floor_division)
+
+print(bitwise_and)
+print(bitwise_or)
+print(bitwise_xor)
+print(bitwise_right_shift)
+print(bitwise_left_shift)
