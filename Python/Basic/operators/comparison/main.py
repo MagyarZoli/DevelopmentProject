8fd5997caac5245350f040ae32cd40564849943e
MagyarZoli <magyarz95@gmail.com>
2024-11-05 06:40:51 +0100

    Comparison #58

---
+++ Python/Basic/operators/comparison/main.py
@@ -0,0 +1,16 @@
+x = 5
+y = 6
+
+equal_to = x == y
+not_equal = x != y
+greater_than = x > y
+less_than = x < y
+greater_than_or_equal_to = x >= y
+less_than_or_equal_to = x <= y
+
+print(equal_to)
+print(not_equal)
+print(greater_than)
+print(less_than)
+print(greater_than_or_equal_to)
+print(less_than_or_equal_to)
