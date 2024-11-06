fda20549c672224f57fd5ecf131dc6d81aadbe01
MagyarZoli <magyarz95@gmail.com>
2024-11-06 06:57:45 +0100

    Identity #59

---
+++ Python/Basic/operators/identity/main.py
@@ -0,0 +1,17 @@
+x = 5
+y = 6
+
+same = x is y
+not_same = x is not y
+
+print(same)
+print(not_same)
+
+x = 3
+y = [1, 2, 3]
+
+present = x in y
+not_present = x not in y
+
+print(present)
+print(not_present)
