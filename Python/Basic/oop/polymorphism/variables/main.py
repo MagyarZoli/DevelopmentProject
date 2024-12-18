82a346cbc8691ba2b6d484167744b0632d2ad660
MagyarZoli <magyarz95@gmail.com>
2024-12-18 07:06:20 +0100

    Polymorphism Variables #68

---
+++ Python/Basic/oop/polymorphism/variables/main.py
@@ -0,0 +1,26 @@
+from b import B
+from c import C
+
+def change_a_field(a_class, a):
+  a_class.a = a
+
+def ab_function(a_class):
+  # a_class.a_function()
+  a_class.b_function()
+
+b = B()
+c = C()
+
+change_a_field(b, "b field")
+change_a_field(c, "c field")
+
+b.b = "b field"
+c.b = "b field"
+
+print(b.a)
+print(c.a)
+print(b.b)
+print(c.b)
+
+ab_function(b)
+ab_function(c)
