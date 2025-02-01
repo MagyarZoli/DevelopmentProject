f4c0ee8f73866e8f9237838f40b80e42075081bc
MagyarZoli <magyarz95@gmail.com>
2025-02-01 08:45:10 +0100

    Chaining Function #84

---
+++ Python/Basic/function/chaining/a.py
@@ -0,0 +1,15 @@
+class A:
+  def function1(self):
+    print("function 1")
+    return self
+
+  def function2(self):
+    print("function 2")
+    return self
+
+  def function3(self):
+    print("function 3")
+    return self
+
+  def function4(self):
+    print("function 4")
---
+++ Python/Basic/function/chaining/main.py
@@ -0,0 +1,9 @@
+from a import A
+
+a = A()
+
+a.function1()\
+    .function2()\
+    .function3()\
+    .function2()\
+    .function4()
