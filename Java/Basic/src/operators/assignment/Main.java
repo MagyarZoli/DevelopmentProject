4551e3c9aa4ad1d28e6a7295d6d6fd248b0c7a42
MagyarZoli <magyarz95@gmail.com>
2025-03-31 08:36:48 +0200

    Assignment #100

---
+++ Java/Basic/src/operators/assignment/Main.java
@@ -0,0 +1,22 @@
+package operators.assignment;
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+
+    // x = y;
+    x += y;
+    // x -= y;
+    // x *= y;
+    // x /= y;
+    // x %= y;
+    // x &= y;
+    // x |= y;
+    // x ^= y;
+    // x >>= y;
+    // x <<= y;
+
+    System.out.println(x);
+  }
+}
