774f3adaaa509f697634d7baa79f5a561c832e7e
MagyarZoli <magyarz95@gmail.com>
2025-05-25 08:44:43 +0200

    This Refers Object #112

---
+++ Java/Basic/src/oop/refersObject/thisx/A.java
@@ -0,0 +1,11 @@
+package oop.refersObject.thisx;
+
+public class A {
+  public int a;
+  public int b;
+
+  public A(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ Java/Basic/src/oop/refersObject/thisx/Main.java
@@ -0,0 +1,10 @@
+package oop.refersObject.thisx;
+
+public class Main {
+  public static void main(String[] args) {
+    A a = new A(3, 4);
+
+    System.out.println(a.a);
+    System.out.println(a.b);
+  }
+}
